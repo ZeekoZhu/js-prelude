534b42eff65d611481c7f00fe3526f298b3a221a
ZeekoZhu <vaezt@outlook.com>
2023-02-12 21:50:22 +0800

    test: make storybook works

---
+++ packages/mobx-reactive-form/src/lib/mobx-reactive-form/react/control-adaptor.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ControlAdaptor } from './';
+
+export default {
+  title: 'ControlAdaptor',
+  component: ControlAdaptor,
+};
+
+export const Default = () => {
+  return <ControlAdaptor>
+    <input />
+  </ControlAdaptor>;
+};
