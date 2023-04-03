2ef257f2691076f91a0f5c42f8bb710f733f87e3
ZeekoZhu <vaezt@outlook.com>
2023-04-03 22:49:51 +0800

    chore: reformat code

--- packages/mobx-reactive-form/.storybook/main.js
+++ packages/mobx-reactive-form/.storybook/main.js
@@ -1,24 +1,27 @@
-const {
-  mergeConfig
-} = require('vite');
+const { mergeConfig } = require('vite');
 const viteTsConfigPaths = require('vite-tsconfig-paths').default;
 module.exports = {
-  stories: ['../src/docs/**/*.stories.mdx', '../src/docs/**/*.stories.tsx', '../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/docs/**/*.stories.mdx',
+    '../src/docs/**/*.stories.tsx',
+    '../src/lib/**/*.stories.mdx',
+    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
-  async viteFinal(config, {
-    configType
-  }) {
+  async viteFinal(config, { configType }) {
     return mergeConfig(config, {
-      plugins: [viteTsConfigPaths({
-        root: '../'
-      })]
+      plugins: [
+        viteTsConfigPaths({
+          root: '../',
+        }),
+      ],
     });
   },
   framework: {
     name: '@storybook/react-vite',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
