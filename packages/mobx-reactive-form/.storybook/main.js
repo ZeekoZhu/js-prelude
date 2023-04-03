40f0993ae6cfc64c517e3a4228622a79cfd7f5e8
ZeekoZhu <vaezt@outlook.com>
2023-04-03 17:45:23 +0800

    build(mobx-reactive-form): upgrade to storybook7

--- packages/mobx-reactive-form/.storybook/main.js
+++ packages/mobx-reactive-form/.storybook/main.js
@@ -1,22 +1,24 @@
-const { mergeConfig } = require('vite');
+const {
+  mergeConfig
+} = require('vite');
 const viteTsConfigPaths = require('vite-tsconfig-paths').default;
-
 module.exports = {
-  core: { builder: '@storybook/builder-vite' },
-  stories: [
-    '../src/docs/**/*.stories.mdx',
-    '../src/docs/**/*.stories.tsx',
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-essentials'],
-  async viteFinal(config, { configType }) {
+  stories: ['../src/docs/**/*.stories.mdx', '../src/docs/**/*.stories.tsx', '../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
+  async viteFinal(config, {
+    configType
+  }) {
     return mergeConfig(config, {
-      plugins: [
-        viteTsConfigPaths({
-          root: '../',
-        }),
-      ],
+      plugins: [viteTsConfigPaths({
+        root: '../'
+      })]
     });
   },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
