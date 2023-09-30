8c58b350aa8b2e8bacdeeebe1e2cdb18c075a215
ZeekoZhu <vaezt@outlook.com>
2023-09-30 22:37:37 +0800

    chore(importify): setup tailwindcss

--- apps/importify/src/app/app.module.ts
+++ apps/importify/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, WelcomeComponent],
+  imports: [BrowserModule, MatButtonModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
