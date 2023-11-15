90f56254d72fd5fcd8687069de5db998435584bc
ZeekoZhu <vaezt@outlook.com>
2023-11-15 22:34:48 +0800

    feat(retro-identicon): improve svg subpixel rendering

--- packages/retro-identicon/src/index.ts
+++ packages/retro-identicon/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/retro-identicon';
+export * from './lib/ascii-render';
+export * from './lib/svg-render';
--- packages/retro-identicon/src/lib/svg-render.ts
+++ packages/retro-identicon/src/lib/svg-render.ts
@@ -1,35 +1,35 @@
 import { IdenticonMatrix } from './retro-identicon';
 
-function createRect(x: number, y: number, fill: string) {
-  return `<rect x="${x}" y="${y}" width="1" height="1" stroke="none" fill="${fill}"/>`;
-}
-
 function proportionalHue(n: number) {
   return Math.ceil(n * 360);
 }
 
+function generateForeground(matrix: number[][], fill: string) {
+  // generate a path for all blocks with value 1
+  const path: string[] = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      const value = matrix[i][j];
+      if (value === 1) {
+        const drawPath = `M${j} ${i} h1 v1 h-1 z`;
+        path.push(drawPath);
+      }
+    }
+  }
+  return `<path d="${path.join(' ')}" fill="${fill}"/>`;
+}
+
 export function svgRender(m: IdenticonMatrix) {
-  const { matrix } = m;
-  const svgTagOpen = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${matrix.length} ${matrix.length}">`;
-  const svgTagClose = '</svg>';
+  const { matrix, hash } = m;
   // take the last 9 bits of the hash as the foreground color hue
-  const fgHue = Number(m.hash & BigInt(0x1ff));
+  const fgHue = Number(hash & BigInt(0x1ff));
   const fgColor = `hsl(${proportionalHue(fgHue / 511)}, 70%, 60%)`;
   // take the next 9 bits of the hash as the background color hue
-  const bgHue = Number((m.hash >> BigInt(9)) & BigInt(0x1ff));
+  const bgHue = Number((hash >> BigInt(9)) & BigInt(0x1ff));
   const bgColor = `hsl(${proportionalHue(bgHue / 511)}, 70%, 40%)`;
-  const rects = matrix
-    .map((row, i) =>
-      row
-        .map((value, j) => {
-          if (value === 0) {
-            // bg block
-            return createRect(j, i, bgColor);
-          }
-          return createRect(j, i, fgColor);
-        })
-        .join(''),
-    )
-    .join('');
-  return svgTagOpen + rects + svgTagClose;
+  const svgTagOpen = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${matrix.length} ${matrix.length}">`;
+  const bgRect = `<rect x="0" y="0" width="${matrix.length}" height="${matrix.length}" stroke="none" fill="${bgColor}"/>`;
+  const svgTagClose = '</svg>';
+  const rects = generateForeground(matrix, fgColor);
+  return svgTagOpen + bgRect + rects + svgTagClose;
 }
