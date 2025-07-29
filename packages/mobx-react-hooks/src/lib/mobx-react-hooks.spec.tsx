4dec52c98283ee7a481d14daeee41f40b6eb2ab5
ZeekoZhu <vaezt@outlook.com>
2025-07-29 11:05:09 +0800

    feat: Add initial MobxReactHooks component

--- packages/mobx-react-hooks/src/lib/mobx-react-hooks.spec.tsx
+++ packages/mobx-react-hooks/src/lib/mobx-react-hooks.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import MobxReactHooks from './mobx-react-hooks';
+import { MobxReactHooks } from './mobx-react-hooks';
 
 describe('MobxReactHooks', () => {
   it('should render successfully', () => {
---
+++ packages/mobx-react-hooks/src/lib/mobx-react-hooks.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function MobxReactHooks() {
+  return <div>Mobx React Hooks</div>;
+}
